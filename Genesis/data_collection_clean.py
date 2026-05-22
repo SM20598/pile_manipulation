ec83dffb546d432a2ac76dba7bb366e246dadf12
Finn Lukas Busch <finn.lukas.busch@gmail.com>
2026-05-23 00:47:07 +0200

    setting sphere/corl data collection params

--- Genesis/data_collection_clean.py
+++ Genesis/data_collection_clean.py
@@ -15,7 +15,7 @@ from sandbox_manipulation_clean import SandboxManipulation
 ##################################
 BASIC_SETTING = "basic"
 DEFAULT_SHAPES = ["sphere"]
-DEFAULT_NUM_PARTICLES = [10]
+DEFAULT_NUM_PARTICLES = [40, 50]
 PARTICLE_SIZES = np.linspace(0.005, 0.012, 5).tolist()
 
 PARTICLE_FRICTIONS = np.linspace(0.05, 0.5, 5).tolist()
